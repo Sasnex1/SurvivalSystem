2c615785e2c0081ef2feac4934f6aebf83dc00ac
Sasnexx <legenaxonymous@gmail.com>
2025-02-24 09:46:25 +0100

    test

---
+++ src/main/java/de/sasnex/survivalSystem/SurvivalSystem.java
@@ -0,0 +1,17 @@
+package de.sasnex.survivalSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
