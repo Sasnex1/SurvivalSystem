6a70ff1f170161849aa0ea7f9d9619f4dcdad753
Sasnexx <legenaxonymous@gmail.com>
2025-02-24 22:37:47 +0100

    Codet by Sasnex

--- src/main/java/de/sasnex/survivalSystem/Commands/HomeCMD.java
+++ src/main/java/de/sasnex/survivalSystem/Commands/HomeCMD.java
@@ -26,97 +26,102 @@ public class HomeCMD implements ICommand, CommandExecutor {
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command cmd, @NonNull String s, @NotNull @NonNull String[] args) {
         if (sender instanceof Player player) {
-            if (args.length >= 1) {
-                String subCommand = args[0].toLowerCase();
+            if (args.length == 0) {
+                // Liste alle gesetzten Homes auf
+                if (fm.getHomesCFG().contains(player.getName() + ".homes")) {
+                    Set<String> homes = Objects.requireNonNull(
+                            fm.getHomesCFG().getConfigurationSection(player.getName() + ".homes")
+                    ).getKeys(false);
 
-                // Zuerst die Subcommands prüfen
-                if (subCommand.equals("list")) {
-                    if (fm.getHomesCFG().contains(player.getName() + ".homes")) {
-                        Set<String> homes = Objects.requireNonNull(
-                                fm.getHomesCFG().getConfigurationSection(player.getName() + ".homes")
-                        ).getKeys(false);
-                        if (!homes.isEmpty()) {
-                            StringBuilder homesList = new StringBuilder(SurvivalSystem.translateChat(
-                                    SurvivalSystem.getPrefix() + "&aDeine Homes:\n"
-                            ));
+                    if (!homes.isEmpty()) {
+                        StringBuilder homesList = new StringBuilder(SurvivalSystem.translateChat(
+                                SurvivalSystem.getPrefix() + "&aDeine Homes:\n"
+                        ));
 
-                            for (String home : homes) {
-                                homesList.append(" &6- &f ").append(home).append("\n");
-                            }
-
-                            player.sendMessage(SurvivalSystem.translateChat(homesList.toString()));
-                        } else {
-                            player.sendMessage(SurvivalSystem.translateChat(
-                                    SurvivalSystem.getPrefix() + "&cDu hast keine Homes gesetzt."
-                            ));
+                        for (String home : homes) {
+                            homesList.append(" &6- &f").append(home).append("\n");
                         }
+
+                        player.sendMessage(SurvivalSystem.translateChat(homesList.toString()));
                     } else {
                         player.sendMessage(SurvivalSystem.translateChat(
                                 SurvivalSystem.getPrefix() + "&cDu hast keine Homes gesetzt."
                         ));
                     }
-                    return true;
-                } else if (subCommand.equals("set") && args.length >= 2) {
-                    String homename = args[1];
-                    Location loc = player.getLocation();
-                    String worldname = Objects.requireNonNull(loc.getWorld()).getName();
+                } else {
+                    player.sendMessage(SurvivalSystem.translateChat(
+                            SurvivalSystem.getPrefix() + "&cDu hast keine Homes gesetzt."
+                    ));
+                }
+                return true;
+            }
 
-                    fm.setHomesData(player.getName() + ".homes." + homename + ".worldname", worldname);
-                    fm.setHomesData(player.getName() + ".homes." + homename + ".x", loc.getX());
-                    fm.setHomesData(player.getName() + ".homes." + homename + ".y", loc.getY());
-                    fm.setHomesData(player.getName() + ".homes." + homename + ".z", loc.getZ());
-                    fm.setHomesData(player.getName() + ".homes." + homename + ".yaw", loc.getYaw());
-                    fm.setHomesData(player.getName() + ".homes." + homename + ".pitch", loc.getPitch());
+            if (args.length >= 2 && args[0].equalsIgnoreCase("set")) {
+                // Setze ein neues Home
+                String homename = args[1];
+                Location loc = player.getLocation();
+                String worldname = Objects.requireNonNull(loc.getWorld()).getName();
 
-                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &6Home wurde gesetzt: &a" + homename));
-                    return true;
-                } else if (subCommand.equals("delete") && args.length >= 2) {
-                    String homename = args[1];
-                    if (fm.homeExists(player.getName(), homename)) {
-                        fm.getHomesCFG().set(player.getName() + ".homes." + homename, null);
-                        fm.saveHomeCFG();
-                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &6Home &a" + homename + " &awurde gelöscht"));
-                    } else {
-                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieses Home existiert nicht."));
+                fm.setHomesData(player.getName() + ".homes." + homename + ".worldname", worldname);
+                fm.setHomesData(player.getName() + ".homes." + homename + ".x", loc.getX());
+                fm.setHomesData(player.getName() + ".homes." + homename + ".y", loc.getY());
+                fm.setHomesData(player.getName() + ".homes." + homename + ".z", loc.getZ());
+                fm.setHomesData(player.getName() + ".homes." + homename + ".yaw", loc.getYaw());
+                fm.setHomesData(player.getName() + ".homes." + homename + ".pitch", loc.getPitch());
+
+                player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &6Home gesetzt: &a" + homename));
+                return true;
+            }
+
+            if (args.length >= 2 && args[0].equalsIgnoreCase("delete")) {
+                // Lösche ein Home
+                String homename = args[1];
+                if (fm.homeExists(player.getName(), homename)) {
+                    fm.getHomesCFG().set(player.getName() + ".homes." + homename, null);
+                    fm.saveHomeCFG();
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &6Home &a" + homename + " &awurde gelöscht"));
+                } else {
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieses Home existiert nicht."));
+                }
+                return true;
+            }
+
+            if (args.length == 1) {
+                // Teleportiere zu einem bestehenden Home
+                String homename = args[0];
+                if (fm.homeExists(player.getName(), homename)) {
+                    String worldname = fm.getHomesCFG().getString(player.getName() + ".homes." + homename + ".worldname");
+                    double x = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".x");
+                    double y = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".y");
+                    double z = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".z");
+                    double yaw = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".yaw");
+                    double pitch = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".pitch");
+
+                    if (worldname == null || Bukkit.getWorld(worldname) == null) {
+                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDie Welt für dieses Home existiert nicht."));
+                        return true;
                     }
+
+                    World world = Bukkit.getWorld(worldname);
+                    Location loc = new Location(world, x, y, z, (float) yaw, (float) pitch);
+
+                    player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aTeleportiere zu Home: &6" + homename));
+
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            player.teleport(loc);
+                            player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
+                            player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aDu wurdest zu deinem Home teleportiert."));
+                        }
+                    }.runTaskLater(SurvivalSystem.getInstance(), 3 * 20L);
                     return true;
                 } else {
-                    // Wenn kein Subcommand, dann als Home-Teleport interpretieren
-                    String homename = args[0];
-                    if (fm.homeExists(player.getName(), homename)) {
-                        String worldname = fm.getHomesCFG().getString(player.getName() + ".homes." + homename + ".worldname");
-                        double x = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".x");
-                        double y = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".y");
-                        double z = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".z");
-                        double yaw = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".yaw");
-                        double pitch = fm.getHomesCFG().getDouble(player.getName() + ".homes." + homename + ".pitch");
-
-                        if (worldname == null || Bukkit.getWorld(worldname) == null) {
-                            player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDie Welt für dieses Home existiert nicht."));
-                            return true;
-                        }
-
-                        World world = Bukkit.getWorld(worldname);
-                        Location loc = new Location(world, x, y, z, (float) yaw, (float) pitch);
-
-                        player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aDu wirst in 3 Sekunden teleportiert &6" + homename));
-
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                player.teleport(loc);
-                                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
-                                player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aDu wurdest zu deinem Home teleportiert."));
-                            }
-                        }.runTaskLater(SurvivalSystem.getInstance(), 3 * 20L);
-                        return true;
-                    } else {
-                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieses Home existiert nicht."));
-                    }
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieses Home existiert nicht."));
                 }
             }
         } else {
-            sender.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cNur Spieler können das ausführen"));
+            sender.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cNur Spieler können diesen Befehl ausführen"));
             return true;
         }
         return false;
--- src/main/java/de/sasnex/survivalSystem/Commands/WarpCMD.java
+++ src/main/java/de/sasnex/survivalSystem/Commands/WarpCMD.java
@@ -14,8 +14,9 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.Set;
 
-public class WarpCMD implements ICommand, CommandExecutor { ;
+public class WarpCMD implements ICommand, CommandExecutor {
 
     public WarpCMD() {
         Objects.requireNonNull(SurvivalSystem.getInstance().getCommand("warp")).setExecutor(this);
@@ -24,84 +25,95 @@ public class WarpCMD implements ICommand, CommandExecutor { ;
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
         if (sender instanceof Player player) {
+            if (args.length == 0) {
+                // Zeige alle gesetzten Warps an
+                if (fm.getWarpsCFG().getKeys(false).isEmpty()) {
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cEs wurden keine Warps gesetzt."));
+                } else {
+                    StringBuilder warpsList = new StringBuilder(SurvivalSystem.translateChat(
+                            SurvivalSystem.getPrefix() + "&aVerfügbare Warps:\n"
+                    ));
+                    for (String warp : fm.getWarpsCFG().getKeys(false)) {
+                        warpsList.append(" &6- &f").append(warp).append("\n");
+                    }
+                    player.sendMessage(warpsList.toString());
+                }
+                return true;
+            }
+
+            if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
+                // Setze einen neuen Warp
+                if (player.hasPermission("ss.setwarp")) {
+                    String warpname = args[1];
+                    Location loc = player.getLocation();
+
+                    fm.setWarpsData(warpname + ".worldname", Objects.requireNonNull(loc.getWorld()).getName());
+                    fm.setWarpsData(warpname + ".x", loc.getX());
+                    fm.setWarpsData(warpname + ".y", loc.getY());
+                    fm.setWarpsData(warpname + ".z", loc.getZ());
+                    fm.setWarpsData(warpname + ".yaw", loc.getYaw());
+                    fm.setWarpsData(warpname + ".pitch", loc.getPitch());
+
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &6Warp gesetzt: &a" + warpname));
+                } else {
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDazu hast du keine Berechtigung!"));
+                }
+                return true;
+            }
+
+            if (args.length == 2 && args[0].equalsIgnoreCase("del")) {
+                // Lösche einen Warp
+                if (player.hasPermission("ss.delwarp")) {
+                    String warpname = args[1];
+                    if (fm.warpExists(warpname)) {
+                        fm.deleteWarp(warpname);
+                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aWarp gelöscht: " + warpname));
+                    } else {
+                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieser Warp existiert nicht!"));
+                    }
+                } else {
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDazu hast du keine Berechtigung!"));
+                }
+                return true;
+            }
+
             if (args.length == 1) {
-                // Teleportation zu einem Warp
-                String warpName = args[0];
-                if (fm.warpExists(warpName)) {
-                    String worldname = fm.getWarpsCFG().getString(warpName + ".worldname");
-                    double x = fm.getWarpsCFG().getDouble(warpName + ".x");
-                    double y = fm.getWarpsCFG().getDouble(warpName + ".y");
-                    double z = fm.getWarpsCFG().getDouble(warpName + ".z");
-                    double yaw = fm.getWarpsCFG().getDouble(warpName + ".yaw");
-                    double pitch = fm.getWarpsCFG().getDouble(warpName + ".pitch");
+                // Teleportiere zu einem Warp
+                String warpname = args[0];
+                if (fm.warpExists(warpname)) {
+                    String worldname = fm.getWarpsCFG().getString(warpname + ".worldname");
+                    double x = fm.getWarpsCFG().getDouble(warpname + ".x");
+                    double y = fm.getWarpsCFG().getDouble(warpname + ".y");
+                    double z = fm.getWarpsCFG().getDouble(warpname + ".z");
+                    double yaw = fm.getWarpsCFG().getDouble(warpname + ".yaw");
+                    double pitch = fm.getWarpsCFG().getDouble(warpname + ".pitch");
 
-                    assert worldname != null;
-                    World world = Bukkit.getWorld(worldname);
-
-                    if (world == null) {
-                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &cDie Welt existiert nicht!"));
+                    if (worldname == null || Bukkit.getWorld(worldname) == null) {
+                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDie Welt existiert nicht!"));
                         return true;
                     }
 
+                    World world = Bukkit.getWorld(worldname);
                     Location loc = new Location(world, x, y, z, (float) yaw, (float) pitch);
 
-                    player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aDu wirst in 3 Sekunden teleportiert"));
+                    player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aTeleportiere zu Warp: &6" + warpname));
 
                     new BukkitRunnable() {
                         @Override
                         public void run() {
                             player.teleport(loc);
                             player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
-                            player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aDu wurdest teleportiert"));
+                            player.sendActionBar(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aDu wurdest teleportiert."));
                         }
                     }.runTaskLater(SurvivalSystem.getInstance(), 3 * 20L);
                 } else {
-                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &cDieser Warp existiert nicht!"));
+                    player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieser Warp existiert nicht!"));
                 }
                 return true;
-            } else if (args[0].equalsIgnoreCase("set")) {
-                //Setzt den Warp
-                if (player.hasPermission("ss.setwarp")) {
-                    if (args.length >= 2) {
-                        String warpname = args[1];
-                        if (!warpname.isEmpty()) {
-                            Location loc = player.getLocation();
-
-                            fm.setWarpsData(warpname + ".worldname", Objects.requireNonNull(loc.getWorld()).getName());
-                            fm.setWarpsData(warpname + ".x", loc.getX());
-                            fm.setWarpsData(warpname + ".y", loc.getY());
-                            fm.setWarpsData(warpname + ".z", loc.getZ());
-                            fm.setWarpsData(warpname + ".yaw", loc.getYaw());
-                            fm.setWarpsData(warpname + ".pitch", loc.getPitch());
-
-                            player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + " &6Warp wurde gesetzt: &a" + warpname));
-                        } else {
-                            player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cKein Warpname angegeben!"));
-                        }
-                    } else {
-                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cBitte gib einen Warpnamen an!"));
-                    }
-                    return true;
-                }
-            } else if (args[0].equalsIgnoreCase("delete")) {
-                //Löscht den Warp | /warp delete <warpname>
-                if (player.hasPermission("ss.deletewarp")) {
-                    if (args.length >= 2) {
-                        String warpname = args[1];
-                        if (fm.warpExists(warpname)) {
-                            fm.deleteWarp(warpname);
-                            player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&aWarp gelöscht: " + warpname));
-                        } else {
-                            player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cDieser Warp existiert nicht!"));
-                        }
-                    } else {
-                        player.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cBitte gib einen Warpnamen an!"));
-                    }
-                    return true;
-                }
             }
+
         } else {
-            sender.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cNur Spieler können diesen Befehl ausführen"));
+            sender.sendMessage(SurvivalSystem.translateChat(SurvivalSystem.getPrefix() + "&cNur Spieler können diesen Befehl ausführen!"));
         }
         return false;
     }
