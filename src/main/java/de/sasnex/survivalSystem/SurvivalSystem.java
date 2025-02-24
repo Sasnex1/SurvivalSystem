c2c3a5087116439aa19e6fb3fdf93415b218b36a
Sasnexx <legenaxonymous@gmail.com>
2025-02-24 09:48:38 +0100

    init commit

--- src/main/java/de/sasnex/survivalSystem/SurvivalSystem.java
+++ src/main/java/de/sasnex/survivalSystem/SurvivalSystem.java
@@ -1,17 +1,38 @@
 package de.sasnex.survivalSystem;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SurvivalSystem extends JavaPlugin {
 
+    SurvivalSystem instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+        sendConsoleMsg("&6SurvivalSystem &aAktiviert");
+        sendConsoleMsg("&eMade by Sasnex");
+    }
+
+    void commands(){
+
+    }
+
+    void listener(){
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        sendConsoleMsg("&6SurvivalSystem &cDeaktiviert");
+    }
+
+    public static String getPrefix() {return "&8» &a&lBreezy&f&lMC &8• ";}
+    public static String translateChat(String msg){return ChatColor.translateAlternateColorCodes('&', msg);}
+    public static void sendConsoleMsg(String msg){
+        Bukkit.getConsoleSender().sendMessage(translateChat(getPrefix()+msg));
     }
 }
